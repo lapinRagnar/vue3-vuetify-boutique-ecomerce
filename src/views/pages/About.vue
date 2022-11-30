9eedef2fe3f851689346a164cf42f77a25a9b557
lapinRagnar <lapinragnar@gmail.com>
2022-11-30 11:51:14 +0100

    installation du vue router > npm install vue-router@4

---
+++ src/views/pages/About.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div>
+    <h1>About</h1>
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/pages/Home.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div>
+    <h1>Home</h1>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
+
